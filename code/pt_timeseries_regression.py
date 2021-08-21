089fb93a2abe4a344cd8911a3a94f82a3b6feff5
RC <c.rohitash@gmail.com>
2021-08-21 17:42:49 +1000

    lhood updated

--- code/pt_timeseries_regression.py
+++ code/pt_timeseries_regression.py
@@ -570,16 +570,7 @@ class ParallelTempering:
         self.use_langevin_gradients = use_langevin_gradients
 
     def default_beta_ladder(self, ndim, ntemps,
-                            Tmax):  # https://github.com/konqr/ptemcee/blob/master/ptemcee/sampler.py
-        """
-        Returns a ladder of :math:`\beta \equiv 1/T` under a geometric spacing that is determined by the
-        arguments ``ntemps`` and ``Tmax``.  The temperature selection algorithm works as follows:
-        Ideally, ``Tmax`` should be specified such that the tempered posterior looks like the prior at
-        this temperature.  If using adaptive parallel tempering, per `arXiv:1501.05823
-        <http://arxiv.org/abs/1501.05823>`_, choosing ``Tmax = inf`` is a safe bet, so long as
-        ``ntemps`` is also specified.
-         
-        """
+                            Tmax):   
 
         if type(ndim) != int or ndim < 1:
             raise ValueError('Invalid number of dimensions specified.')
@@ -589,28 +580,7 @@ class ParallelTempering:
             raise ValueError('``Tmax`` must be greater than 1.')
         if ntemps is not None and (type(ntemps) != int or ntemps < 1):
             raise ValueError('Invalid number of temperatures specified.')
-
-        # tstep = np.array([25.2741, 7., 4.47502, 3.5236, 3.0232,
-        #                   2.71225, 2.49879, 2.34226, 2.22198, 2.12628,
-        #                   2.04807, 1.98276, 1.92728, 1.87946, 1.83774,
-        #                   1.80096, 1.76826, 1.73895, 1.7125, 1.68849,
-        #                   1.66657, 1.64647, 1.62795, 1.61083, 1.59494,
-        #                   1.58014, 1.56632, 1.55338, 1.54123, 1.5298,
-        #                   1.51901, 1.50881, 1.49916, 1.49, 1.4813,
-        #                   1.47302, 1.46512, 1.45759, 1.45039, 1.4435,
-        #                   1.4369, 1.43056, 1.42448, 1.41864, 1.41302,
-        #                   1.40761, 1.40239, 1.39736, 1.3925, 1.38781,
-        #                   1.38327, 1.37888, 1.37463, 1.37051, 1.36652,
-        #                   1.36265, 1.35889, 1.35524, 1.3517, 1.34825,
-        #                   1.3449, 1.34164, 1.33847, 1.33538, 1.33236,
-        #                   1.32943, 1.32656, 1.32377, 1.32104, 1.31838,
-        #                   1.31578, 1.31325, 1.31076, 1.30834, 1.30596,
-        #                   1.30364, 1.30137, 1.29915, 1.29697, 1.29484,
-        #                   1.29275, 1.29071, 1.2887, 1.28673, 1.2848,
-        #                   1.28291, 1.28106, 1.27923, 1.27745, 1.27569,
-        #                   1.27397, 1.27227, 1.27061, 1.26898, 1.26737,
-        #                   1.26579, 1.26424, 1.26271, 1.26121,
-        #                   1.25973])
+ 
 
         maxtemp = Tmax
         numchain = ntemps
